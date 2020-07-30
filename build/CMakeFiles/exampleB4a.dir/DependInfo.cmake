8e330b9937ce880b0c2280d3eb62532bfcd6ae6a
Jiawei_Guo <gjwei1999@163.com>
2020-07-30 21:48:38 +0800

    upload

---
+++ build/CMakeFiles/exampleB4a.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jiaweiguo/work/de_e/exampleB4a.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4DetectorConstruction.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4DetectorConstruction.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4PrimaryGeneratorAction.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4RunAction.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4RunAction.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4aActionInitialization.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4aActionInitialization.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4aEventAction.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4aEventAction.cc.o"
+  "/home/jiaweiguo/work/de_e/src/B4aSteppingAction.cc" "/home/jiaweiguo/work/de_e/build/CMakeFiles/exampleB4a.dir/src/B4aSteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/jiaweiguo/apps/GEANT4/geant4-install/include/Geant4"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
